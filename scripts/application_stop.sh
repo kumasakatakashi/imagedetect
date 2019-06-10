b39d5e0f2b7440accb3fe99959816fdf5a2749ed
Takashi Kumasaka <downsoul5150@gmail.com>
2019-06-10 15:54:14 +0900

    Update application_stop.sh

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-isExistApp = `pgrep php-fpm`
-if [[ -n  $isExistApp ]]; then
-    service php-fpm stop
-fi
+#isExistApp = `pgrep php-fpm`
+#if [[ -n  $isExistApp ]]; then
+#    service php-fpm stop
+#fi
