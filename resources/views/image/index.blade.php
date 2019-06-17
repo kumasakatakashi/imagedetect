76491d37c5bcc13aef3b55f9ec32129866c19a8d
Takashi Kumasaka <downsoul5150@gmail.com>
2019-06-17 11:24:54 +0900

    Update index.blade.php

--- resources/views/image/index.blade.php
+++ resources/views/image/index.blade.php
@@ -13,7 +13,7 @@
                 <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
-                <ul>
+                <ul style="margin-bottom:0px">
                     @foreach($errors->all() as $error)
                     <li>{{ $error }}</li>
                     @endforeach
