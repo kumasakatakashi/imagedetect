74949d81ee43c1db7373a03c1f12279161df8f0f
Takashi Kumasaka <downsoul5150@gmail.com>
2019-06-18 16:58:07 +0900

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -13,5 +13,7 @@
 Auth::routes();
 Route::get('/', 'ImageController@index');
 Route::post('upload', 'ImageController@upload');
+Route::get('upload', 'ImageController@index'); //エラー用
 Route::post('detect', 'ImageController@detect');
+Route::get('detect', 'ImageController@index'); //エラー用
 Route::redirect('/home', '/');
