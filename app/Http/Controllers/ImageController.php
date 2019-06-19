2382d5651fa1284c32cd01aa6142d5f17551d5c1
Takashi Kumasaka <downsoul5150@gmail.com>
2019-06-19 12:00:21 +0900

    Update ImageController.php

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Support\Api\GoogleVisionApi;
 
 class ImageController extends Controller
 {
@@ -44,49 +45,11 @@ class ImageController extends Controller
      */
     public function detect(Request $request)
     {
-        // APIキー
-        $api_key = config('google.apikey');
-
-        // 認識対象の画像パス
         $image_path = $request->input('imagepath');
 
-        // リクエスト用のJSONを作成
-        $json = json_encode([
-            "requests" => [
-                [
-                    "image" => [
-                        "content" => base64_encode(file_get_contents($image_path)),
-                    ],
-                    "features" => [
-                        [
-                            "type" => "DOCUMENT_TEXT_DETECTION" ,
-                            "maxResults" => 10 ,
-                        ],
-                    ],
-                ],
-            ],
-        ]);
-
-        // リクエストを実行
-        $curl = curl_init();
-        curl_setopt($curl, CURLOPT_URL, "https://vision.googleapis.com/v1/images:annotate?key=" . $api_key);
-        curl_setopt($curl, CURLOPT_HEADER, true);
-        curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "POST");
-        curl_setopt($curl, CURLOPT_HTTPHEADER, array("Content-Type: application/json"));
-        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-        if(isset($referer) && !empty($referer)) curl_setopt($curl, CURLOPT_REFERER, $referer);
-        curl_setopt($curl, CURLOPT_TIMEOUT, 15);
-        curl_setopt($curl, CURLOPT_POSTFIELDS, $json);
-        $res1 = curl_exec($curl);
-        $res2 = curl_getinfo($curl);
-        curl_close($curl);
-
-        // 取得したデータ
-        $json = substr($res1, $res2["header_size"]);
-        $array_json=json_decode($json, true);
-
-        $text=$array_json["responses"]["0"]["textAnnotations"]["0"]["description"];
+        // OCR実行 by GoogleVisionAPI
+        $api = new GoogleVisionApi();
+        $text = $api->document_text_detection($image_path);
 
         return view('image.index')->with([
             'filename' => basename($image_path),
