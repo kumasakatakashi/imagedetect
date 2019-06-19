5a8519ef1a6d29d60cbe50092ccfa8aa48f3f2e3
Takashi Kumasaka <downsoul5150@gmail.com>
2019-06-19 11:59:49 +0900

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -10,10 +10,17 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-Auth::routes();
+// Root
 Route::get('/', 'ImageController@index');
-Route::post('upload', 'ImageController@upload');
-Route::get('upload', 'ImageController@index'); //エラー用
-Route::post('detect', 'ImageController@detect');
-Route::get('detect', 'ImageController@index'); //エラー用
 Route::redirect('/home', '/');
+
+// Auth
+Auth::routes();
+
+// UploadImage
+Route::post('upload', 'ImageController@upload');
+Route::get('upload', 'ImageController@index');
+
+// DetectTextFromImage
+Route::post('detect', 'ImageController@detect');
+Route::get('detect', 'ImageController@index');
