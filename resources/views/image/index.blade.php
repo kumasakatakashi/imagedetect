f61713a343efd68450f8d34d6eaae9f9b8865722
Takashi Kumasaka <downsoul5150@gmail.com>
2019-06-21 18:13:21 +0900

    Update index.blade.php

--- resources/views/image/index.blade.php
+++ resources/views/image/index.blade.php
@@ -40,14 +40,14 @@
                 <!-- アップロード画像表示 -->
                 @isset($filepath)
                 <hr>
-                <a href="{{ url($filepath) }}" data-lightbox="detectimage">
-                    <img src="{{ url($filepath) }}" width="200" height="150" />
+                <a href="{{ Storage::url($filepath) }}" data-lightbox="detectimage">
+                    <img src="{{ Storage::url($filepath) }}" width="200" height="150" />
                 </a>
                 <!-- OCR認識フォーム -->
                 <hr>
                 {!! Form::open(['url' => '/detect', 'method' => 'post', 'files' => false]) !!}
                 <div class="form-group">
-                    {!! Form::hidden('filepath', url($filepath)) !!}
+                    {!! Form::hidden('filepath', Storage::url($filepath)) !!}
                     {!! Form::submit('文字認識', ['class' => 'btn btn-danger']) !!}
                 </div>
                 {!! Form::close() !!}
